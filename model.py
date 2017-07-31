859d89d1fb699edbd9a44c8eb4a3226d5ef15df6
sama18-meet <sama.mazz93@gmail.com>
2017-07-31 22:49:16 +0300

    Made the databases

--- model.py
+++ model.py
@@ -4,14 +4,30 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-User(Base):
+class User(Base):
 	__tablename__= 'user'
 	id = Column(Integer, primary_key= True)
 	full_name= Column(String)
 	username= Column(String)
 	password = Column(String)
 	museum_choice = Column(String)
+	post = relationship("Post")
     # ADD YOUR FIELD BELOW ID
+
+class Post(Base):
+	__tablename__= 'post'
+	id = Column(Integer, primary_key=True)
+	title = Column(String)
+	museum-Music = Column(Boolean)
+	museum-Photography = Column(Boolean)
+	museum-Painting = Column(Boolean)
+	description = Column(String)
+	pic_url = Column(String)
+	likes = Column(Integer)
+	user_id = Column(Integer, ForeignKey('user.id'))
+
+
+
 
 # IF YOU NEED TO CREATE OTHER TABLE 
 # FOLLOW THE SAME STRUCTURE AS YourModel
