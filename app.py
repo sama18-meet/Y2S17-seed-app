459ff9d72faed23ad09d557ac14f9329ac2fa89e
sama18-meet <sama.mazz93@gmail.com>
2017-08-01 12:22:06 +0300

    add login

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 # flask imports
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, Response, render_template, request, redirect, url_for
+
+# flask setup
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ITSASECRET"
+
+# flask-login imports
+from flask_login import login_required, current_user
+from login import login_manager, login_handler, logout_handler
+login_manager.init_app(app)
 
 # SQLAlchemy
-from model import Base, YourModel
+from model import Base, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-# setup
-app = Flask(__name__)
 engine = create_engine('sqlite:///project.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
@@ -17,3 +23,19 @@ session = DBSession()
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return login_handler(request)
+
+
+@app.route('/logout')
+def logout():
+  return logout_handler()
+
+
+@app.route('/protected', methods=["GET"])
+@login_required
+def protected():
+    return render_template('protected.html')
--- model.py
+++ model.py
@@ -1,26 +1,43 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from flask_login import UserMixin
+
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+from sqlalchemy.orm import relationship
+
+from werkzeug.security import generate_password_hash, check_password_hash
 Base = declarative_base()
 
 
-class User(Base):
-	__tablename__= 'user'
-	id = Column(Integer, primary_key= True)
-	full_name= Column(String)
-	username= Column(String)
-	password = Column(String)
-	museum_choice = Column(String)
-	post = relationship("Post")
-    # ADD YOUR FIELD BELOW ID
+class User(UserMixin, Base):
+    __tablename__ = 'user'
+    id            = Column(Integer, primary_key=True)
+    email         = Column(String)
+    pw_hash       = Column(String)
+    authenticated = Column(Boolean, default=False)
+    museum_museum = Column(Boolean)
+    museum_photography = Column(Boolean)
+    museum_painting = Column(Boolean)
+    post = relationship("Post")
+
+    def __repr__(self):
+      return "<User: %s, password: %s>" % (
+        self.email, self.pw_hash)
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+
 
 class Post(Base):
 	__tablename__= 'post'
 	id = Column(Integer, primary_key=True)
 	title = Column(String)
-	museum-Music = Column(Boolean)
-	museum-Photography = Column(Boolean)
-	museum-Painting = Column(Boolean)
+	museum_music = Column(Boolean)
+	museum_photography = Column(Boolean)
+	museum_painting = Column(Boolean)
 	description = Column(String)
 	pic_url = Column(String)
 	likes = Column(Integer)
